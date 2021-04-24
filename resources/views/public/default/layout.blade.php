83ebf83e842667bc682453eb234d88b3897e3874
Irfan <muhirfan234@gmail.com>
2021-04-24 19:35:24 +0700

    tambah page

--- resources/views/public/default/layout.blade.php
+++ resources/views/public/default/layout.blade.php
@@ -78,7 +78,7 @@
                 <div class="heading"><h1>Hubungi Kami</h1></div>
                 <div class="description">Siap Buat Website ? Hubungi Kami Sekarang</div>
                 <div class="description2"><a href="https://api.whatsapp.com/send?phone=6287734611551&amp;text=Hello Idcodewebs, saya mau buat website...">  <img class="margin-right-1" src="{{ theme_asset_public('frontend::default', 'img/whatsapp.svg') }}" width="24" alt="idcodewebs-contact-us" style="vertical-align: sub">087734611551</a></div>
-                <div class="description2">Rt. 02 Rw.04 Drono, Drono, Ngawen, Klaten</div>
+                <div class="description2"></div>
             </div>
         </div>
         <div class="peta">
@@ -87,28 +87,21 @@
     </div>
     <footer>
         <div>
-            <h4>Kontak</h4>
+            <h4>Idcodewebs</h4>
             <p>
-                I can feel a phoenix inside of me. Maybe a reason why all the doors are closed.
-                We go higher and higher. Passport stamps, she's cosmopolitan. Someone said you had your tattoo removed.
-                All my girls vintage Chanel baby. Someone said you had your tattoo removed.
+                Jasa Pembuatan Website di Klaten. Pembuatan Website Paket Wordpress, Toko Online dan Paket Laravel untuk Instansi.
             </p>            
         </div>
         <div>
             
-            <h4>Menu</h4>
+            <h4>Kontak</h4>
             <div class="space-y-2">
                 <ul class="contact">
                     <li><p><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" width="16" height="16" stroke="currentColor">
                       <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z"></path>
                       <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 11a3 3 0 11-6 0 3 3 0 016 0z"></path>
-                  </svg> Jl. Marsda Adisucipto, Yogyakarta, Indonesia 55281 <!--  --></p></li>
-                    <li><p><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" width="16" height="16" stroke="currentColor">
-                      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z"></path>
-                    </svg> <strong>Phone:</strong> +62-87734611551<!--  --></p></li>
-                    <li><p><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" width="16" height="16" stroke="currentColor">
-                      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"></path>
-                    </svg> <strong>Email:</strong> <a href="mailto:humas@uin-suka.ac.id.com"><!--   -->info@domain.com</a></p></li>
+                  </svg> Rt. 02 Rw.04 Drono, Drono, Ngawen, Klaten</p></li>
+                   
                 </ul>
             </div>
         </div>
